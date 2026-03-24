f70c4a934b222f0da2f3b4063f0d804b3439b1a3
Your Name <rishirk0422@gmail.com>
2026-03-24 12:00:08 +0530

    second fe commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://greencart-backend-5evx.onrender.com"
 });
 
 // ✅ attach token to every request
